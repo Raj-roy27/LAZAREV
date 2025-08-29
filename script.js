6c5ab94ed1b4da3e70ff19a7316752935af348e0
Raj Roy <royr938559@gmail.com>
2025-08-30 00:00:07 +0530

    project Lazarev in progress

---
+++ script.js
@@ -0,0 +1,40 @@
+function navAnimation(){
+    let nav = document.querySelector("nav");
+    let navPart2 = document.querySelector(".nav-part2");
+nav.addEventListener("mouseenter", () => {
+  let tl = gsap.timeline();
+  tl.to("#nav-bottom", {
+    height: "23vh",
+  });
+  tl.to("nav .nav-part2 h5", {
+    display: "block",
+  });
+  tl.to(".nav-part2 h5 span", {
+    y: 0,
+    stagger: {
+      amount: 0.7,
+    },
+  });
+});
+
+nav.addEventListener("mouseleave", () => {
+  let tl = gsap.timeline();
+
+  tl.to(".nav-part2 h5 span", {
+    y: 25,
+    stagger: {
+      amount: 0.2,
+    },
+  });
+  tl.to(".nav-part2 h5", {
+    display: "none",
+    duration: 0.1,
+  });
+  tl.to("#nav-bottom", {
+    height: "0vh",
+    // duration: 0.2,
+  });
+});
+}
+
+navAnimation();
