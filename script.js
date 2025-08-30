aedcae8216a4fcd1bf78e8739ca3d0d44ae059b1
Raj Roy <royr938559@gmail.com>
2025-08-30 14:45:29 +0530

    Page 3 Video feature Added

--- script.js
+++ script.js
@@ -37,8 +37,6 @@ function navAnimation() {
   });
 }
 
-navAnimation();
-
 function page2Animation() {
   let rightElems = document.querySelectorAll(".right-elem");
   rightElems.forEach((e) => {
@@ -65,4 +63,42 @@ function page2Animation() {
     });
   });
 }
+
+function page3Animation(){
+  let page3Center = document.querySelector(".page3-center");
+let page3Video = document.querySelector(".page-3 video");
+let page3 = document.querySelector(".page-3");
+
+page3Center.addEventListener("click", () => {
+  page3Video.play();
+  gsap.to(page3Video, {
+    transform: "scaleX(1) scaleY(1)",
+    display: "block",
+    borderRadius: 0,
+    duration: 2.5,
+    ease: "power4.out",
+    opacity: 1,
+  });
+});
+
+page3Video.addEventListener("click", ()=>{
+  page3Video.pause();
+  gsap.to(page3Video, {
+    transform: "scaleX(0.7) scaleY(0.2)",
+    display: "none",
+    borderRadius: 30,
+    duration: 1,
+    ease: "power4.out",
+    opacity: 0,
+  });
+})
+}
+
+
+navAnimation();
 page2Animation();
+page3Animation();
+
+
+
+
